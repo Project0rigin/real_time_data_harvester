bc7f30afabf269e1dc8158a61101c6dcbc364008
Lourens Kok <lourenskok@gmail.com>
2024-04-29 22:25:36 +0200

    updated binance api config to include actual urls and added websocket api settings in config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,12 @@
-# Binance API
-BINANCE_BASE_URL = ''
-BINANCE_BALANCE_ENDPOINT = ''
-BINANCE_SYMBOL_PRICE_TICKER_ENDPOINT = ''
+# Binance Base API settings
+BASE_URL = "https://api.binance.com"
+
+# Binance Market Data API settings
+CHECK_SERVER_TIME_URL_ENDPOINT__MARKETDATA = "/api/v3/time"
+EXCHANGE_INFO_URL_ENDPOINT__MARKETDATA = "/api/v3/exchangeInfo"
+ACCOUNT_URL_ENDPOINT__MARKETDATA = "/api/v3/account"
+SYMBOL_PRICE_TICKER_URL_ENDPOINT__MARKETDATA = "/api/v3/ticker/price"
+
+# Binance Websocket API settings
+WS_BASE_URL = "wss://stream.binance.com:9443/ws"
+WS_TICKER_URL_ENDPOINT = "/btcusdt@ticker"
