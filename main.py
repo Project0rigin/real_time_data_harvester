101befca28db8cdbe0b0c052d00ec6b5e8cda00f
Lourens Kok <lourenskok@gmail.com>
2024-04-30 19:27:54 +0200

    cleaned up and commented out unused exchanges

--- main.py
+++ main.py
@@ -8,44 +8,15 @@ from src.config import BINANCE_WEBSOCKET_URI
 async def main():
     # Initialize exchange objects
     binance = Binance()
-    coinbase = Coinbase()
-    valr = Valr()
+    # coinbase = Coinbase()
+    # valr = Valr()
 
     # Run each exchange's listener concurrently
     await asyncio.gather(
         binance.listen_for_trades(BINANCE_WEBSOCKET_URI),
-        coinbase.listen_for_trades("TODO"),
-        valr.listen_for_trades("TODO")
+        # coinbase.listen_for_trades("TODO"),
+        # valr.listen_for_trades("TODO")
     )
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# TODO set own API key and move to relevant exchange instances (get api key etc from Jack)
-# def on_message(ws, message):
-#     message = json.loads(message)
-#     print("Received message: ", message)
-
-# def on_error(ws, error):
-#     print("Error: ", error)
-
-# def on_close(ws):
-#     print("### closed ###")
-
-# def on_open(ws):
-#     def run(*args):
-#         ws.send(json.dumps({'method': 'SUBSCRIBE', 'params': ['btcusdt@ticker'], 'id': 1}))
-#     return run
-
-# if __name__ == "__main__":
-#     while True:
-#         websocket.enableTrace(True)
-#         ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/btcusdt@ticker",
-#                                     on_open=on_open,
-#                                     on_message=on_message,
-#                                     on_error=on_error,
-#                                     on_close=on_close)
-#         ws.run_forever()
-#         print("Reconnecting...")
-#         # Sleep for 10 seconds before reconnecting after websocket disconnected
-#         time.sleep(10)
