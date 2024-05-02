cd0a3482547cf113db772bc3c04c86498c294b03
Lourens Kok <lourenskok@gmail.com>
2024-05-02 22:50:18 +0200

    added fetch symbole ticker price but it is not correct market price as oppose to live websocket feed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.data_collectors.binance import Binance
 from src.config import BINANCE_WEBSOCKET_URI
 from src.config import COINS
 from src.config import SYMBOL_PRICE_TICKER_URL_ENDPOINT__MARKETDATA
+from src.config import BASE_URL
 
 from dotenv import load_dotenv
 import os
@@ -16,21 +17,20 @@ binance_api_key = os.getenv("BINANCE_API_KEY")
 binance_api_secret = os.getenv("BINANCE_API_SECRET")
 asset_pairs = COINS
 symbol_price_ticker_endpoint = SYMBOL_PRICE_TICKER_URL_ENDPOINT__MARKETDATA
+base_url = BASE_URL
 
 async def main():
     # Initialize exchange objects
-    binance = Binance(binance_api_key, binance_api_secret, asset_pairs)
+    binance = Binance(base_url, binance_api_key, binance_api_secret, asset_pairs)
+    binance.get_current_price(symbol_price_ticker_endpoint)
     # coinbase = Coinbase()
     # valr = Valr()
 
-    # Run each exchange's listener concurrently
     await asyncio.gather(
         binance.listen_for_trades(BINANCE_WEBSOCKET_URI),
         # coinbase.listen_for_trades("TODO"),
         # valr.listen_for_trades("TODO")
     )
-
-# binance.get_price('BTC', symbol_price_ticker_endpoint)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- src/data_collectors/binance.py
+++ src/data_collectors/binance.py
@@ -5,7 +5,7 @@ import logging
 
 #TODO build on websocket logic
 class Binance:
-    def __init__(self, base_url, api_key, asset_pairs):
+    def __init__(self, base_url, api_key, api_secret, asset_pairs):
         self.base_url = base_url
         self.api_key = api_key
         self.asset_pairs = asset_pairs
@@ -16,24 +16,17 @@ class Binance:
                 message = await websocket.recv()
                 print("Received message:", message)
 
-    # def get_price(self, ticker, symbol_price_ticker_endpoint):
-    #     try:
-    #         response = requests.get(
-    #             f'{self.base_url}{symbol_price_ticker_endpoint}?symbol={ticker}',
-    #             headers = {
-    #             'X-MBX-APIKEY': self.api_key,
-    #         }
-    #         )
-    #         response.raise_for_status()
-    #         data = response.json()
-    #         price = data.get("price")
-    #         if price:
-    #             logging.info("Price for %s: %s", ticker, price)
-    #             return price
-    #         else:
-    #             logging.info("No price information found for %s", ticker)
-    #             return None
-    #     except requests.RequestException as e:
-    #         logging.error("Error retrieving price for %s: %s", str(e))
-    #         return None
-# /{self.asset_pairs[1]}@trade
+    def get_current_price(self, symbol_price_ticker_endpoint):
+        print(f"Retrieving current price for {self.asset_pairs[0]}")
+        try:
+            response = requests.get(
+                f"{self.base_url}{symbol_price_ticker_endpoint}?symbol={self.asset_pairs[0].upper()}",
+                headers={"X-MBX-APIKEY": self.api_key},
+            )
+            response.raise_for_status()
+            price = response.json()["price"]
+            print(f"Current price for {self.asset_pairs[0]}: {price}")
+            return price
+        except requests.RequestException as e:
+            print(f"Error retrieving current price for {self.asset_pairs[0]}: {str(e)}")
+            return None
