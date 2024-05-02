d20bc27fc9f170c6f7033bd111535b83a5b8eba1
Lourens Kok <lourenskok@gmail.com>
2024-05-03 01:16:29 +0200

    working market price json api for all give coins in config

--- src/data_collectors/binance.py
+++ src/data_collectors/binance.py
@@ -10,11 +10,18 @@ class Binance:
         self.base_url = base_url
         self.api_key = api_key
         self.asset_pairs = asset_pairs
-        self.last_message = {}
+        self.last_message = {pair: None for pair in asset_pairs}
 
     async def listen_for_trades(self, ws_url):
         async with websockets.connect(f"{ws_url}{concat_trade_name(self.asset_pairs)}") as websocket:
             while True:
                 message = await websocket.recv()
-                self.last_message = json.loads(message)
+                data = json.loads(message)
                 print("Received message:", message)
+                if 'stream' in data:
+                    asset_pair = data['stream'].split('@')[0]
+                    price = data['data']['p'] if 'p' in data['data'] else None
+                    if asset_pair in self.last_message:
+                        self.last_message[asset_pair] = price
+                    else:
+                        print(f"Received message for unknown asset pair: {asset_pair}")
